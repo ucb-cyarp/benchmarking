23155ee3859c723776d28fd90a608bcebce4066e
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-02-26 18:12:13 -0800

    Adding intrinsics benchmarking

---
+++ intrin/intrin_throughput_test.cpp
@@ -0,0 +1,127 @@
+/*
+ * Benchmark for SSE/AVX/FMA Vector Unit
+ * 
+ * Test execution speeds for SSE/AVX/FMA for different
+ * operations and datatypes.
+ * 
+ * Compiler Parameters (defined as macros)
+ *     - STIM_LEN:     Stimulus Length
+ *     - TRIALS:       Number of Trial Runs
+ *     - PRINT_TITLE:  1 to Print Title Text
+ *     - PRINT_HEADER: 1 to Print Test Header
+ *     - PRINT_TRIALS: 1 to Print Each Trial
+ *     - PRINT_STATS:  1 to Print Statistics
+ * 
+ * Arguments
+ *     
+ */
+
+#include <chrono>
+#include <ctime>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <cstdint>
+
+//Include Intrinsics
+#include <x86intrin.h>
+#include <immintrin.h>
+#include <mm_malloc.h>
+// #include <mmintrin.h>
+// #include <emmintrin.h>
+// #include <xmmintrin.h>
+
+//Set default options
+#ifndef STIM_LEN
+    #define STIM_LEN 160000000
+#endif
+
+#ifndef TRIALS
+    #define TRIALS 10
+#endif
+
+#ifndef PRINT_TITLE
+    #define PRINT_TITLE 1
+#endif
+
+#ifndef PRINT_HEADER
+    #define PRINT_HEADER 1
+#endif
+
+#ifndef PRINT_TRIALS
+    #define PRINT_TRIALS 1
+#endif
+
+#ifndef PRINT_STATS
+    #define PRINT_STATS 1
+#endif
+
+void test_mm256_add_epi8()
+{
+    #if PRINT_HEADER == 1
+        printf("Add 32 Packed 8 bit Signed Integers (_mm256_add_epi8)\n");
+    #endif
+
+    for(int trial = 0; trial<TRIALS; trial++)
+    {
+        //Allocate timer arrays
+        std::chrono::duration<double, std::ratio<1, 1000>> durations[TRIALS];
+        double durations_clock[TRIALS];
+        double durations_rdtsc[TRIALS];
+
+        //Allocate the arrays to operate over
+        //These arrays need to be aligned to the vector register size of 256a
+        void* a = _mm_malloc (STIM_LEN*sizeof(int8_t), 32);
+        void* b = _mm_malloc (STIM_LEN*sizeof(int8_t), 32);
+        void* c = _mm_malloc (STIM_LEN*sizeof(int8_t), 32);
+
+        __m256i* a_m256 = (__m256i * ) a;
+        __m256i* b_m256 = (__m256i * ) b;
+        __m256i* c_m256 = (__m256i * ) c;
+
+        //Start Timer
+        std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+        clock_t start_clock = clock();
+        uint64_t start_rdtsc = _rdtsc();
+
+        //Execute
+        for(int i = 0; i<STIM_LEN/32; i++)
+        {
+            __m256i a_val = _mm256_load_si256(a_m256+i);
+            __m256i b_val = _mm256_load_si256(b_m256+i);
+            __m256i c_val = _mm256_add_epi8(a_val, b_val);
+        }
+
+        //Stop Timer and Report Time
+        uint64_t stop_rdtsc = _rdtsc();
+        clock_t stop_clock = clock();
+        std::chrono::high_resolution_clock::time_point stop = std::chrono::high_resolution_clock::now();
+
+        durations[trial] = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(stop-start);
+        durations_clock[trial] = 1000.0 * (stop_clock - start_clock) / CLOCKS_PER_SEC;
+        durations_rdtsc[trial] =  (stop_rdtsc - start_rdtsc);
+
+        #if PRINT_TRIALS == 1
+            printf("Trial %6d: %f, %f, %f\n", trial, durations[trial], durations_clock[trial], durations_rdtsc[trial]);
+        #endif 
+
+        //Clean up
+        _mm_free(a);
+        _mm_free(b);
+        _mm_free(c);
+    }
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    #if PRINT_TITLE == 1
+    printf("SSE/AVX/FMA Intrinsic Tester\n");
+    printf("STIM_LEN: %d, TRIALS: %d\n", STIM_LEN, TRIALS);
+    #endif
+
+    test_mm256_add_epi8();
+}
