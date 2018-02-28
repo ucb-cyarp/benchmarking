1e01abc11ac4a60e057a14b34b904ee33feb3c89
cyarp <cyarp@eecs.berkeley.edu>
2018-02-28 14:07:54 -0800

    Refactored add unroll 2 kernels into own file.

--- intrin/load_add_store_unroll2.cpp
+++ intrin/load_add_store_unroll2.cpp
@@ -1,26 +1,9 @@
 #include "intrin_bench_default_defines.h"
+#include "load_add_store_unroll2_kernel.h"
 
 #include "statistics.h"
 
 //==========_mm256_add_epi8==========
-void kernel_u2_mm256_add_epi8( __m256i* a, __m256i* b, __m256i* c)
-{
-    for(int i = 0; i<STIM_LEN/32; i+=2)
-    {
-        __m256i a_val  = _mm256_load_si256(a+i);
-        __m256i b_val  = _mm256_load_si256(b+i);
-        __m256i c_val  = _mm256_add_epi8(a_val, b_val);
-        _mm256_store_si256(c+i, c_val);
-
-        int i2 = i+1;
-
-        __m256i a2_val = _mm256_load_si256(a+i2);
-        __m256i b2_val = _mm256_load_si256(b+i2);
-        __m256i c2_val = _mm256_add_epi8(a2_val, b2_val);
-        _mm256_store_si256(c+i2, c2_val);
-    }
-}
-
 void test_u2_mm256_add_epi8()
 {
     #if PRINT_HEADER == 1
@@ -76,24 +59,6 @@ void test_u2_mm256_add_epi8()
 }
 
 //==========_mm256_add_epi16==========
-void kernel_u2_mm256_add_epi16( __m256i* a, __m256i* b, __m256i* c)
-{
-    for(int i = 0; i<STIM_LEN/16; i+=2)
-    {
-        __m256i a_val = _mm256_load_si256(a+i);
-        __m256i b_val = _mm256_load_si256(b+i);
-        __m256i c_val = _mm256_add_epi16(a_val, b_val);
-        _mm256_store_si256(c+i, c_val);
-
-        int i2 = i+1;
-
-        __m256i a2_val = _mm256_load_si256(a+i2);
-        __m256i b2_val = _mm256_load_si256(b+i2);
-        __m256i c2_val = _mm256_add_epi16(a2_val, b2_val);
-        _mm256_store_si256(c+i2, c2_val);
-    }
-}
-
 void test_u2_mm256_add_epi16()
 {
     #if PRINT_HEADER == 1
@@ -149,24 +114,6 @@ void test_u2_mm256_add_epi16()
 }
 
 //==========_mm256_add_epi32==========
-void kernel_u2_mm256_add_epi32( __m256i* a, __m256i* b, __m256i* c)
-{
-    for(int i = 0; i<STIM_LEN/8; i+=2)
-    {
-        __m256i a_val = _mm256_load_si256(a+i);
-        __m256i b_val = _mm256_load_si256(b+i);
-        __m256i c_val = _mm256_add_epi32(a_val, b_val);
-        _mm256_store_si256(c+i, c_val);
-
-        int i2 = i+1;
-
-        __m256i a2_val = _mm256_load_si256(a+i2);
-        __m256i b2_val = _mm256_load_si256(b+i2);
-        __m256i c2_val = _mm256_add_epi32(a2_val, b2_val);
-        _mm256_store_si256(c+i2, c2_val);
-    }
-}
-
 void test_u2_mm256_add_epi32()
 {
     #if PRINT_HEADER == 1
@@ -222,28 +169,6 @@ void test_u2_mm256_add_epi32()
 }
 
 //==========_mm256_add_ps==========
-void kernel_u2_mm256_add_ps( __m256* a, __m256* b, __m256* c)
-{
-    float* a_float = (float*) a;
-    float* b_float = (float*) b;
-    float* c_float = (float*) c;
-
-    for(int i = 0; i<STIM_LEN; i+=16)
-    {
-        __m256 a_val = _mm256_load_ps(a_float+i);
-        __m256 b_val = _mm256_load_ps(b_float+i);
-        __m256 c_val = _mm256_add_ps(a_val, b_val);
-        _mm256_store_ps(c_float+i, c_val);
-
-        int i2 = i+8;
-
-        __m256 a2_val = _mm256_load_ps(a_float+i2);
-        __m256 b2_val = _mm256_load_ps(b_float+i2);
-        __m256 c2_val = _mm256_add_ps(a_val, b_val);
-        _mm256_store_ps(c_float+i2, c2_val);
-    }
-}
-
 void test_u2_mm256_add_ps()
 {
     #if PRINT_HEADER == 1
@@ -299,28 +224,6 @@ void test_u2_mm256_add_ps()
 }
 
 //==========_mm256_add_pd==========
-void kernel_u2_mm256_add_pd( __m256d* a, __m256d* b, __m256d* c)
-{
-    double* a_double = (double*) a;
-    double* b_double = (double*) b;
-    double* c_double = (double*) c;
-
-    for(int i = 0; i<STIM_LEN; i+=8)
-    {
-        __m256d a_val = _mm256_load_pd(a_double+i);
-        __m256d b_val = _mm256_load_pd(b_double+i);
-        __m256d c_val = _mm256_add_pd(a_val, b_val);
-        _mm256_store_pd(c_double+i, c_val);
-
-        int i2 = i+4;
-
-        __m256d a2_val = _mm256_load_pd(a_double+i2);
-        __m256d b2_val = _mm256_load_pd(b_double+i2);
-        __m256d c2_val = _mm256_add_pd(a2_val, b2_val);
-        _mm256_store_pd(c_double+i2, c2_val);
-    }
-}
-
 void test_u2_mm256_add_pd()
 {
     #if PRINT_HEADER == 1
