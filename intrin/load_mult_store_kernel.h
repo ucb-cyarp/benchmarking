459fdcf372dd8ee7734c2107d97edc5eee2156b1
cyarp <cyarp@eecs.berkeley.edu>
2018-03-09 12:24:47 -0800

    Work on benchmarking while considering cache size

--- intrin/load_mult_store_kernel.h
+++ intrin/load_mult_store_kernel.h
@@ -3,18 +3,9 @@
 
     #include "intrin_bench_default_defines.h"
 
-    #ifdef __AVX2__
-        //==========_mm256_mullo_epi16==========
-        void kernel_mm256_mullo_epi16( __m256i* a, __m256i* b, __m256i* c);
-        //==========_mm256_mullo_epi32==========
-        void kernel_mm256_mullo_epi32( __m256i* a, __m256i* b, __m256i* c);
-    #endif
+    #define KERNEL_LEN = STIM_LEN
+    #define KERNEL_PREFIX = 
 
-    #ifdef __AVX__
-        //==========_mm256_mul_ps==========
-        void kernel_mm256_mul_ps( __m256* a, __m256* b, __m256* c);
-        //==========_mm256_mul_pd==========
-        void kernel_mm256_mul_pd( __m256d* a, __m256d* b, __m256d* c);
-    #endif
+    #include "load_mult_store_kernel_header_template.h"
 
 #endif
