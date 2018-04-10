3840b3ca46804366fc880c4ec848e564373eea27
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-04-09 20:15:59 -0700

    forgot to add actual kenel code for dual shared pointer kernel

---
+++ core-core/latency_dual_kernel.h
@@ -0,0 +1,37 @@
+#ifndef _H_LATENCY_DUAL_KERNEL
+    #define _H_LATENCY_DUAL_KERNEL
+    #include "cpucounters.h"
+
+    // class LatencySingleKernelServerArgs
+    // {
+    //     public:
+    //         PCM* pcm;
+    //         int32_t* shared_ptr;
+    // };
+
+    // class LatencySingleKernelClientArgs
+    // {
+    //     public:
+    //         int32_t* shared_ptr;
+    // };
+
+    class LatencyDualKernelArgs
+    {
+        public:
+            int32_t* my_shared_ptr;
+            int32_t* other_shared_ptr;
+            int32_t init_counter;
+    };
+
+    class LatencyDualKernelResetArgs
+    {
+        public:
+            int32_t* shared_ptr_a;
+            int32_t* shared_ptr_b;
+    };
+
+    void* latency_dual_kernel(void* arg);
+
+    void* latency_dual_kernel_reset(void* arg);
+    
+#endif
