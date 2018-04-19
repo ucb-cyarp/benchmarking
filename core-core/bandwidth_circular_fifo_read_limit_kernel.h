a65c689654922e9f95c34cd370e6d73d886eea7a
cyarp <cyarp@eecs.berkeley.edu>
2018-04-18 19:45:42 -0700

    Adding FIFO read limit kernel

---
+++ core-core/bandwidth_circular_fifo_read_limit_kernel.h
@@ -0,0 +1,21 @@
+#ifndef _H_BANDWIDTH_CIRCULAR_FIFO_READ_LIMIT_KERNEL
+    #define _H_BANDWIDTH_CIRCULAR_FIFO_READ_LIMIT_KERNEL
+    #include "cpucounters.h"
+
+    class BandwidthCircularFifoReadLimitKernelArgs
+    {
+        public:
+            int32_t* array_shared_ptr;
+            int32_t* write_pos_shared_ptr;
+            int32_t* read_pos_shared_ptr;
+            int32_t max_elements_per_transaction;
+            size_t length;
+    };
+
+    void* bandwidth_circular_fifo_read_limit_server_kernel(void* arg);
+
+    void* bandwidth_circular_fifo_read_limit_client_kernel(void* arg);
+
+    void* bandwidth_circular_fifo_read_limit_kernel_reset(void* arg);
+    
+#endif
