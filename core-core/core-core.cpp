26d4efd8cc0152bf39db3a25f6d08b9871e5aa5a
cyarp <cyarp@eecs.berkeley.edu>
2018-04-18 18:17:01 -0700

    Accounting for block lengths that do not perfectly divide the stim lenght when calculating statistics.  Accounting for the extra elements

--- core-core/core-core.cpp
+++ core-core/core-core.cpp
@@ -159,6 +159,68 @@ void print_results(Results* results, int bytes_per_transfer, int stim_len, int l
     #endif
 }
 
+void print_results_blocked_fifo(Results* results, int bytes_per_transfer, int target_stim_len, int block_length)
+{
+    //The actual stim length will actually be larger if the block length does not equally divide the stim length.
+    //Does one more block in this case which fills the remaining slots and adds a few more.
+    //The number of unfilled slots without the extra block is target_stim_len%block_len.
+    //The amount over the target stim length with the extra block is the block_len - unfilled slots
+    int stim_len = target_stim_len;
+    if(target_stim_len%block_length != 0)
+    {
+        stim_len = target_stim_len + block_length - target_stim_len%block_length;
+    }
+
+    double avg_duration_ms = results->avg_duration();
+    double stddev_duration_ms = results->stddev_duration();
+    double avg_latency_ns = avg_duration_ms*1000000/stim_len;
+    double stddev_latency_ns = stddev_duration_ms*1000000/stim_len;
+
+    double transactions_rate_msps = stim_len/(1000.0*avg_duration_ms);
+
+    double data_rate_mbps = 8.0*bytes_per_transfer*stim_len/(1000.0*avg_duration_ms);
+
+    printf("        =======================================================\n");
+    printf("        Metric                   |     Avg      |    StdDev    \n");
+    printf("        =======================================================\n");
+    //printf("        Duration (ms)            |%14.6f|%14.6f\n", stim_len, avg_duration_ms, stddev_duration_ms);
+    printf("        One Way Latency (ns)     |%14.6f|%14.6f\n", avg_latency_ns, stddev_latency_ns);
+    printf("        Transaction Rate (MT/s)  |%14.6f|\n", transactions_rate_msps);
+    printf("        Data Rate (Mbps)         |%14.6f|\n", data_rate_mbps);
+    printf("        =======================================================\n");
+}
+
+void print_results_blocked_fifo(Results* results, int bytes_per_transfer, int target_stim_len, int length, int block_length, std::string format, FILE* file=NULL, std::ofstream* raw_file=NULL)
+{
+    //The actual stim length will actually be larger if the block length does not equally divide the stim length.
+    //Does one more block in this case which fills the remaining slots and adds a few more.
+    //The number of unfilled slots without the extra block is target_stim_len%block_len.
+    //The amount over the target stim length with the extra block is the block_len - unfilled slots
+    int stim_len = target_stim_len;
+    if(target_stim_len%block_length != 0)
+    {
+        stim_len = target_stim_len + block_length - target_stim_len%block_length;
+    }
+
+    double avg_duration_ms = results->avg_duration();
+    double stddev_duration_ms = results->stddev_duration();
+    double avg_latency_ns = avg_duration_ms*1000000/stim_len;
+    double stddev_latency_ns = stddev_duration_ms*1000000/stim_len;
+
+    double transactions_rate_msps = stim_len/(1000.0*avg_duration_ms);
+
+    double data_rate_mbps = 8.0*bytes_per_transfer*stim_len/(1000.0*avg_duration_ms);
+
+    #if PRINT_STATS == 1
+    printf(format.c_str(), data_rate_mbps);
+    #endif
+
+    #if WRITE_CSV == 1
+    fprintf(file, "%f", data_rate_mbps);
+    results->write_durations(*raw_file, "", length, "", block_length, false);
+    #endif
+}
+
 Results* run_latency_single_kernel(PCM* pcm, int cpu_a, int cpu_b)
 {
     //=====Test 1=====
@@ -1007,7 +1069,7 @@ Results* run_bandwidth_fifo_blocked_kernel(PCM* pcm, int cpu_a, int cpu_b, size_
                     #endif
 
                     #if PRINT_STATS == 1
-                    print_results(results, sizeof(*shared_array_loc), STIM_LEN);
+                    print_results_blocked_fifo(results, sizeof(*shared_array_loc), STIM_LEN, block_length);
                     #endif
 
             #else
@@ -1015,12 +1077,12 @@ Results* run_bandwidth_fifo_blocked_kernel(PCM* pcm, int cpu_a, int cpu_b, size_
                     results->print_statistics(0, cpu_a, STIM_LEN);
                     #endif
 
-                    print_results(results, sizeof(*shared_array_loc), STIM_LEN); //Div by 2 is because the counter increments for each direction of the FIFO transaction (transmit and ack)
+                    print_results_blocked_fifo(results, sizeof(*shared_array_loc), STIM_LEN, block_length); //Div by 2 is because the counter increments for each direction of the FIFO transaction (transmit and ack)
             #endif
         }
         else
         {
-            print_results(results, sizeof(*shared_array_loc), STIM_LEN, array_length, block_length, format, file, raw_file); //Div by 2 is because the counter increments for each direction of the FIFO transaction (transmit and ack)
+            print_results_blocked_fifo(results, sizeof(*shared_array_loc), STIM_LEN, array_length, block_length, format, file, raw_file); //Div by 2 is because the counter increments for each direction of the FIFO transaction (transmit and ack)
         }
     #endif
 
