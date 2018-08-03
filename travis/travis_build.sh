ffd1416df138f153e751390d31cff71fac014628
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-08-02 18:31:39 -0700

    Adding echos to build and acually supplying a valid exit code

--- travis/travis_build.sh
+++ travis/travis_build.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
+#Using tecnique from http://bencane.com/2014/09/02/understanding-exit-codes-and-how-to-use-them-in-bash-scripts/
 if [ "$(uname)" == "Darwin" ]; then
     cd ../intrin
     make -f Makefile-noPCM
+
+    if [ $? -eq 0 ]
+    then
+        echo "****Successfully made intrinsic benchmark with no PCM ****"
+        exit 0
+    else
+        echo "****Unable to make intrinsic benchmark with no PCM ****" >&2
+        exit 1
+    fi
 else
     cd ../intrin
     make
+    if [ $? -eq 0 ]
+    then
+        echo "****Successfully made intrinsic benchmark with PCM ****"
+        exit 0
+    else
+        echo "****Unable to make intrinsic benchmark with PCM ****" >&2
+        exit 1
+    fi
+
     make clean
+        if [ $? -eq 0 ]
+    then
+        echo "****Successfully cleaned intrinsic benchmark ****"
+        exit 0
+    else
+        echo "****Unable to clean intrinsic benchmark ****" >&2
+        exit 1
+    fi
+
     make -f Makefile-noPCM
+    if [ $? -eq 0 ]
+    then
+        echo "****Successfully made intrinsic benchmark with no PCM ****"
+        exit 0
+    else
+        echo "****Unable to make intrinsic benchmark with no PCM ****" >&2
+        exit 1
+    fi
+
     cd ../core-core
     make
+    if [ $? -eq 0 ]
+    then
+        echo "****Successfully made core-core benchmark****"
+        exit 0
+    else
+        echo "****Unable to make core-core benchmark****" >&2
+        exit 1
+    fi
 fi
